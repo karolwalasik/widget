7d7a215cdaed1dd4ec174ac7835c4a3cc603533d
karolwalasik <karol.walasik@gmail.com>
2018-09-16 13:13:59 +0200

    zmiana w php

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-$(document).ready(function () {
-	$.ajax({
-		method: 'GET',
-		url: '../dbh.php',
-		dataType: 'json'
-	}).done(function (data) {
-		var random = Math.floor(Math.random() * data.users.length);
-		$("#name").text(data.users[random].name);
-		$("#lastname").text(data.users[random].lastname);
-		$("#company").text(data.users[random].company);
-
-	});
-});
+console.log("elo2");
